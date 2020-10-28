9ae62903319a285993c1dade4c3306dbd83d9d7b
HBeate <hauser_b@hotmail.com>
2020-10-28 19:49:57 +0100

    first commit

---
+++ Wörterbuch/Wörterbuch.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Wörterbuch
+{
+    partial class Wörterbuch
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbGermanWord = new System.Windows.Forms.TextBox();
+            this.tbEnglishWord = new System.Windows.Forms.TextBox();
+            this.lbTranslation = new System.Windows.Forms.Label();
+            this.lBoxGermanWords = new System.Windows.Forms.ListBox();
+            this.tbTranslation = new System.Windows.Forms.TextBox();
+            this.btnExportToCsv = new System.Windows.Forms.Button();
+            this.tbSearchGerman = new System.Windows.Forms.TextBox();
+            this.lbSearchGermanWeord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(324, 94);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 51);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Hinzufügen";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbGermanWord
+            // 
+            this.tbGermanWord.Location = new System.Drawing.Point(12, 55);
+            this.tbGermanWord.Name = "tbGermanWord";
+            this.tbGermanWord.Size = new System.Drawing.Size(325, 22);
+            this.tbGermanWord.TabIndex = 1;
+            // 
+            // tbEnglishWord
+            // 
+            this.tbEnglishWord.Location = new System.Drawing.Point(463, 55);
+            this.tbEnglishWord.Name = "tbEnglishWord";
+            this.tbEnglishWord.Size = new System.Drawing.Size(325, 22);
+            this.tbEnglishWord.TabIndex = 2;
+            // 
+            // lbTranslation
+            // 
+            this.lbTranslation.AutoSize = true;
+            this.lbTranslation.Location = new System.Drawing.Point(365, 58);
+            this.lbTranslation.Name = "lbTranslation";
+            this.lbTranslation.Size = new System.Drawing.Size(70, 17);
+            this.lbTranslation.TabIndex = 3;
+            this.lbTranslation.Text = "DE <> EN";
+            // 
+            // lBoxGermanWords
+            // 
+            this.lBoxGermanWords.FormattingEnabled = true;
+            this.lBoxGermanWords.ItemHeight = 16;
+            this.lBoxGermanWords.Location = new System.Drawing.Point(42, 170);
+            this.lBoxGermanWords.Name = "lBoxGermanWords";
+            this.lBoxGermanWords.Size = new System.Drawing.Size(353, 196);
+            this.lBoxGermanWords.TabIndex = 4;
+            this.lBoxGermanWords.SelectedIndexChanged += new System.EventHandler(this.lBoxGermanWords_SelectedIndexChanged);
+            // 
+            // tbTranslation
+            // 
+            this.tbTranslation.Location = new System.Drawing.Point(463, 181);
+            this.tbTranslation.Name = "tbTranslation";
+            this.tbTranslation.Size = new System.Drawing.Size(324, 22);
+            this.tbTranslation.TabIndex = 5;
+            // 
+            // btnExportToCsv
+            // 
+            this.btnExportToCsv.Location = new System.Drawing.Point(425, 313);
+            this.btnExportToCsv.Name = "btnExportToCsv";
+            this.btnExportToCsv.Size = new System.Drawing.Size(109, 53);
+            this.btnExportToCsv.TabIndex = 6;
+            this.btnExportToCsv.Text = "Liste speichern";
+            this.btnExportToCsv.UseVisualStyleBackColor = true;
+            this.btnExportToCsv.Click += new System.EventHandler(this.btnExportToTxt_Click);
+            // 
+            // tbSearchGerman
+            // 
+            this.tbSearchGerman.Location = new System.Drawing.Point(221, 466);
+            this.tbSearchGerman.Name = "tbSearchGerman";
+            this.tbSearchGerman.Size = new System.Drawing.Size(255, 22);
+            this.tbSearchGerman.TabIndex = 7;
+            this.tbSearchGerman.TextChanged += new System.EventHandler(this.tbSearchGerman_TextChanged);
+            // 
+            // lbSearchGermanWeord
+            // 
+            this.lbSearchGermanWeord.AutoSize = true;
+            this.lbSearchGermanWeord.Location = new System.Drawing.Point(39, 466);
+            this.lbSearchGermanWeord.Name = "lbSearchGermanWeord";
+            this.lbSearchGermanWeord.Size = new System.Drawing.Size(159, 17);
+            this.lbSearchGermanWeord.TabIndex = 8;
+            this.lbSearchGermanWeord.Text = "Deutsches Wort suchen";
+            // 
+            // Wörterbuch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 623);
+            this.Controls.Add(this.lbSearchGermanWeord);
+            this.Controls.Add(this.tbSearchGerman);
+            this.Controls.Add(this.btnExportToCsv);
+            this.Controls.Add(this.tbTranslation);
+            this.Controls.Add(this.lBoxGermanWords);
+            this.Controls.Add(this.lbTranslation);
+            this.Controls.Add(this.tbEnglishWord);
+            this.Controls.Add(this.tbGermanWord);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Wörterbuch";
+            this.Text = "4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbGermanWord;
+        private System.Windows.Forms.TextBox tbEnglishWord;
+        private System.Windows.Forms.Label lbTranslation;
+        private System.Windows.Forms.ListBox lBoxGermanWords;
+        private System.Windows.Forms.TextBox tbTranslation;
+        private System.Windows.Forms.Button btnExportToCsv;
+        private System.Windows.Forms.TextBox tbSearchGerman;
+        private System.Windows.Forms.Label lbSearchGermanWeord;
+    }
+}
+
---
+++ Wörterbuch/Wörterbuch.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Wörterbuch
+{
+
+    public partial class Wörterbuch : Form
+    {
+        Dictionary<string, string> germanToEnglishDict = new Dictionary<string, string>();
+        string dictionaryPath = "C:\\Users\\DCV\\Desktop\\Coding\\04_Vertiefung_CSharp\\Wörterbuch\\Wörterbuch\\dictionary.txt";
+  //      String[] englishCSVPath = System.IO.File.ReadAllLines(@"C:\Users\DCV\Desktop\Coding\04_Vertiefung_CSharp\Wörterbuch\Wörterbuch\dictionary.txt");
+        public Wörterbuch()
+        {
+            InitializeComponent();
+            importDictionaryTxt();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var germanWord = tbGermanWord.Text; //.Text -> liefert den Inhalt der Textbox
+            var englishWord = tbEnglishWord.Text;
+
+            if(!string.IsNullOrEmpty(germanWord) && !string.IsNullOrEmpty(englishWord))  //schat, ob die Felder nicht leer sind
+            {
+                germanToEnglishDict.Add(germanWord, englishWord);
+                UpdateTranslations();
+                tbGermanWord.Clear();
+                tbEnglishWord.Clear();
+            }
+        }
+        private void UpdateTranslations()
+        {
+            lBoxGermanWords.DataSource = germanToEnglishDict.Keys.ToList();
+        }
+        private void lBoxGermanWords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedWord = lBoxGermanWords.SelectedItem as string; 
+
+            if (!string.IsNullOrEmpty(selectedWord) && germanToEnglishDict.ContainsKey(selectedWord))
+            {
+                tbTranslation.Text = germanToEnglishDict[selectedWord];
+            }   
+        }
+
+        private void btnExportToTxt_Click(object sender, EventArgs e)
+        {
+            using (var writer = new StreamWriter(dictionaryPath))
+            {
+                foreach (var pair in germanToEnglishDict)
+                {
+                    writer.WriteLine("{0};{1};", pair.Key, pair.Value);
+                }
+            }
+        }
+
+        public void importDictionaryTxt()
+        {
+            var vocs = File.ReadLines(dictionaryPath)
+                   .Select(line => line.Split(';'))
+                   .Select(x => new { german = x[0], others = x[1] });
+
+            germanToEnglishDict.Clear();
+
+            vocs.ToList().ForEach(x => germanToEnglishDict.Add(x.german, x.others));
+            
+
+                UpdateTranslations();
+        }
+
+        private void tbSearchGerman_TextChanged(object sender, EventArgs e)
+        {
+            Dictionary<string, string> searchGermanWordsDict = new Dictionary<string, string>();
+            
+        //wordToSearch
+            string searchLetters = tbSearchGerman.Text;
+
+            foreach (var pair in this.germanToEnglishDict)
+            {
+                if (pair.Key.ToLower().Contains(searchLetters.ToLower()))
+                {
+                    searchGermanWordsDict.Add(pair.Key, pair.Value);
+                }
+            }
+            updateSearchBox(searchGermanWordsDict);
+        }
+
+        public void updateSearchBox(Dictionary<string, string> germanToEnglishDict)
+        {
+            lBoxGermanWords.DataSource = germanToEnglishDict.Keys.ToList();
+        }
+
+
+    }
+}
